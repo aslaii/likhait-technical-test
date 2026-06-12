3ec4b264b762357f931300c96f330c394b7c869b
Jericho Bermas <jecho.deleon@gmail.com>
2026-06-12 09:26:56 +0800

    feat(expenses): validate expense date is not in the future

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validates :date, presence: true
+  validates :date, comparison: { less_than_or_equal_to: -> { Date.current + 1.day } }
 end
